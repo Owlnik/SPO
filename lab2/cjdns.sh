3020505002dd97e071554571756ee3d90cd9f4f6
Owlink <owlstydy@gmail.com>
2020-12-02 13:30:04 +0300

    lab7

--- lab2/cjdns.sh
+++ lab2/cjdns.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+if test -z `whitch docker` then
+	echo "install docker" && exit
+fi
 sudo apt-get update 
 sudo apt-get install -y git nodejs build-essential python2.7
 git clone "https://github.com/cjdelisle/cjdns" || (echo "error cloning project" && exit)
